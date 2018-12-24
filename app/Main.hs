3d6b18f4211c0f5dc00b111f20695bc2c9c4c7a8
Bulat Motygullin <bulat.motygullin@interactive.film>
2018-12-25 02:55:26 +0300

    Починил

--- app/Main.hs
+++ app/Main.hs
@@ -127,7 +127,7 @@ placeMark (i, j) game = do
       Nothing -> castIO game -- если поставить фишку нельзя, ничего не изменится
       Just newBoard -> castIO game
         { gameBoard  = newBoard
-        , gamePlayer = gamePlayer
+        , gamePlayer = switchPlayer (gamePlayer game)
         , gameWinner = winner newBoard
         }
 
